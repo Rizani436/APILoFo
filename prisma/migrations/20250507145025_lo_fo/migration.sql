e8cb788496ea5ba95092bc135858afec30128b35
Aniiiii <rizani4366@gmail.com>
2025-05-07 22:53:48 +0800

    update

---
+++ prisma/migrations/20250507145025_lo_fo/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "BarangHilang" ALTER COLUMN "tanggalHilang" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "BarangTemuan" ALTER COLUMN "tanggalTemuan" SET DATA TYPE TEXT;
