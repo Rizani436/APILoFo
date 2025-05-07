cc1cf56780d1d1f95166be12014a34068702b194
Aniiiii <rizani4366@gmail.com>
2025-05-07 12:48:08 +0800

    update

--- src/barang-temuan/barang-temuan.controller.ts
+++ src/barang-temuan/barang-temuan.controller.ts
@@ -50,6 +50,36 @@ export class BarangTemuanController {
   ) {
     return this.BarangTemuanService.create(dataData, file);
   }
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const uniqueName = `${Date.now()}-${Math.round(
+            Math.random() * 1e9,
+          )}${extname(file.originalname)}`;
+          cb(null, uniqueName);
+        },
+      }),
+    }),
+  )
+  async uploadFile(
+    @UploadedFile() file: MulterFile,
+    @Body('nama') nama: string,
+  ) {
+    const url = `http://localhost:3000/uploads/${file.filename}`;
+
+    const data = {
+      nama: nama,
+      pictUrl: url,
+    };
+
+    return {
+      message: 'Upload & simpan data berhasil',
+      data,
+    };
+  }
 
   @Get('getAll')
   async getAll() {
--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,17 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import * as cors from 'cors';
 import * as express from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  // Serve static files dari /uploads
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
 
-  app.use(
-    cors({
-      origin: 'http://localhost:5173',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      credentials: true,
-      allowedHeaders: ['Content-Type', 'Authorization'],
-    }),
-  );
-
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-
-  app.use(cookieParser());
-
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`🚀 Application is running on: ${await app.getUrl()}`);
+  await app.listen(3000);
 }
 bootstrap();
